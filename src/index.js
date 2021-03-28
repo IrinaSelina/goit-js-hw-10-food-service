954f7befca83c8be4a8e5e12355b6f5b0e63c015
Irina Selina <iras198308@gmail.com>
2021-03-29 00:37:31 +0300

    Убирает лишнюю разметку

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import cards from './menu.json';
 const jsMenu = document.querySelector('.js-menu')
 const body = document.querySelector('body')
 const input = document.querySelector('.theme-switch__toggle')
-const makeMenuCard = menuCard(cards);
+// const makeMenuCard = menuCard(cards);
+const makeMenuCard = cardsTemplate(cards);
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 jsMenu.insertAdjacentHTML('beforeend', makeMenuCard)
-function menuCard(cards) {
-   return cardsTemplate(cards)
-}
+// function menuCard(cards) {
+//    return cardsTemplate(cards)
+// }
+
 body.classList.add("light-theme")
 
 function changeTheme(evn) {
@@ -20,13 +23,13 @@ function changeTheme(evn) {
       if (input.checked) {
          body.classList.replace('light-theme', 'dark-theme')
          localStorage.setItem('theme', body.className)
-         localStorage.setItem('check', input.checked)
+         // localStorage.setItem('check', input.checked)
          return                    
       }
       if (!input.checked) {
          body.classList.replace('dark-theme', 'light-theme')
          localStorage.setItem('theme', body.className)
-         localStorage.setItem('check', input.checked)
+         // localStorage.setItem('check', input.checked)
       }
    }
    }    
@@ -36,11 +39,14 @@ function changeTheme(evn) {
 function chek() {
    if (localStorage.getItem('theme') === Theme.DARK) {
       body.classList.toggle(Theme.DARK)
-      input.checked = localStorage.getItem('check')
+      // input.checked = localStorage.getItem('check')
+     input.checked = true
       return
    }
+  
    return
    }
  
 chek()
+
 
